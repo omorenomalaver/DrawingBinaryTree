d122a0b2cde62c81329faaf17e9b281a6a7b37c9
Oscar Moreno <omorenomalaver@gmail.com>
2016-12-01 14:02:28 +0800

    first version

---
+++ shapes/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace shapes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawingPanel = new System.Windows.Forms.Panel();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DrawingPanel
+            // 
+            this.DrawingPanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DrawingPanel.Location = new System.Drawing.Point(12, 38);
+            this.DrawingPanel.Name = "DrawingPanel";
+            this.DrawingPanel.Size = new System.Drawing.Size(1200, 600);
+            this.DrawingPanel.TabIndex = 0;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(12, 9);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 1;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 350);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.DrawingPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel DrawingPanel;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ shapes/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shapes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            drawTree(6);
+        }
+
+        public void drawTree(int levels)
+        {
+            int panelW = DrawingPanel.Size.Width;
+            int panelH = DrawingPanel.Size.Height;
+
+            SolidBrush sb = new SolidBrush(Color.Green);
+            Pen myPen = new Pen(Color.Red);
+            Graphics g = DrawingPanel.CreateGraphics();
+
+            int xPos = panelW / 2;
+            int yPos = 60;
+            int width = 20;
+            int height = 20;
+            int number_nodes = 1;
+            int space = xPos;
+            int startPosition = xPos;
+            
+
+            for (int i = 1; i<= levels; i++)
+            {
+                number_nodes = number_nodes * 2;
+            }
+            number_nodes = number_nodes - 1;
+            Console.WriteLine("******* number nodes" + number_nodes);
+            int counter = 1;
+            int number_nodes_by_level = 2;
+
+            
+            int[,] values = new int [number_nodes, 2];
+
+            while (number_nodes != 0)
+            {
+                if (counter > 1)
+                {
+                    
+                    if (number_nodes_by_level == counter)
+                    {
+                        yPos = yPos + 100;
+                        space = panelW / (number_nodes_by_level + 1);
+                        xPos = space;
+                        number_nodes_by_level = number_nodes_by_level * 2;
+                    }
+                    else
+                    {
+                        xPos = xPos + space;
+                    }
+                }
+                Label node = new Label();
+                node.Location = new Point(xPos, yPos);
+                node.Text = "(x:" + xPos + ") (y:" + yPos+")";
+
+                //DrawingPanel.Controls.Add(node);
+                Rectangle rectangle = new Rectangle(xPos, yPos, width, height);
+                g.DrawEllipse(myPen, rectangle);
+                values[counter - 1, 0] = xPos;
+                values[counter - 1, 1] = yPos;
+
+                counter++;
+                number_nodes--;
+            }
+            /*
+            int maxLines = (values.Length / 2) -1;
+            int yPoxS = 0,xPosS = 0;
+            for (int i = 0; i<= maxLines; i++)
+            {
+                if (yPoxS == 0)
+                {
+                    xPosS = values[i, 0];
+                    yPoxS = values[i, 1];
+                }
+                else
+                {
+
+                    System.Drawing.Graphics graphicsObj;
+
+                    graphicsObj = this.CreateGraphics();
+
+                    g.DrawLine(myPen, xPosS, yPoxS, values[i, 0], values[i, 1]);
+                    if (yPoxS != values[i, 1] && i>2)
+                    {
+                        xPosS = values[i, 0];
+                        yPoxS = values[i, 1];
+                    }
+                    else
+                    {
+                        xPosS = values[i, 0];
+                    }
+                }
+                Console.WriteLine("x:" + values[i, 0]);
+                Console.WriteLine("y:" + values[i, 1]);
+
+                
+
+            }
+            */
+            /*for (int i = 1; i<=levels; i++)
+            {
+                int tempNumberNodes = number_nodes;
+                
+                while (tempNumberNodes != 0)
+                {
+                    Rectangle rectangle = new Rectangle(xPos, yPos, width, height);
+                    g.DrawEllipse(myPen, rectangle);
+                    xPos = xPos + (startPosition * number_nodes);
+                    tempNumberNodes--;
+                }
+                yPos = yPos + 100;
+                xPos = startPosition / 2;
+                startPosition = xPos;
+                number_nodes = number_nodes * 2;
+                
+            }
+            */
+
+
+        }
+    }
+}
